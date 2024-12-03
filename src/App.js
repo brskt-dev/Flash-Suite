063f54590818a2301aa6b95b4878396414e86d5d
Bruno <brunoanhezini@hotmail.com>
2024-12-03 12:28:37 -0300

    Added Theme Switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import React, {useState, useEffect} from 'react';
 import { GeistProvider, CssBaseline, Page } from '@geist-ui/core';
+
 import FlashInterface from './components/FlashInterface';
 
 const App = () => {
+
+  const [themeType, setThemeType] = useState('dark');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme') || 'dark';
+    setThemeType(storedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const nextTheme = themeType === 'light' ? 'dark' : 'light';
+    setThemeType(nextTheme);
+    localStorage.setItem('theme', nextTheme);
+  };
+
   return (
-    <GeistProvider>
+    <GeistProvider themeType={themeType}>
       <CssBaseline />
       <Page>
-        <FlashInterface />
+        <FlashInterface toggleTheme={toggleTheme} currentTheme={themeType} />
       </Page>
     </GeistProvider>
   );
